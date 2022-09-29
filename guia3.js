8bfe227d29b51664e8b67bd27222d806c0ed0f44
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-28 21:45:34 -0500

    ejercicio 3

--- guia3.js
+++ guia3.js
@@ -1,20 +1,26 @@
 addEventListener("DOMContentLoaded",()=>{
-let cantPro=0
-while(cantPro<10){
-    let n1=Number(prompt("ingrese un numero "))
-    let n2= Number(prompt("ingrese otro numero"))
-    let suma = n1 + n2
-    let multiplicacion = n1 * n2
-    let resta = n1 - n2
-    let division= n1/n2
-    console.log(`${n1} + ${n2}  = ${suma}`)
-    console.log(`${n1} * ${n2}  = ${multiplicacion}`)
-    console.log(`${n1} - ${n2}  = ${resta}`)
-    console.log(`${n1} / ${n2}  = ${division}`)
+let a = 0
+while ( a < 5){
+    let nombre = prompt("Ingrese el nombre y apellido: ")
+    let edad = Number(prompt("Ingrese su edad: "))
+    let nota = Number(prompt("Ingrese su nota: "))
+    console.log(nombre)
+    console.log(edad)
+    console.log(nota)
+
     let p= prompt("coloca p para para o cualquier lettra para continuar")
     if (p=='p'){
-        break
-    }
+        break}
 
 }
 })
+// '''
+// Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de
+// computación.
+// '''
+// a = 0
+// while a < 5:
+//     nombre = input("Ingrese el nombre y apellido: ")
+//     edad = int(input("Ingrese su edad: "))
+//     nota = float(input("Ingrese su nota: "))
+//     a+=1
